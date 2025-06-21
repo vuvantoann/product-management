1ece6ba26b2bfaff986b4987276e7ef42ba58f57
vuvantoann <toanvu1728@gamil.com>
2025-06-21 07:24:03 +0700

    Tính năng xóa sản phẩm khỏi giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -67,3 +67,19 @@ module.exports.addPost = async (req, res) => {
   req.flash('success', 'Bạn thêm sản phẩm vào giỏ hàng thành công.')
   res.redirect(req.get('Referer') || '/')
 }
+
+// [GET] cart/delete/productId
+module.exports.delete = async (req, res) => {
+  const productId = req.params.productId
+  const cartId = req.cookies.cartId
+
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    { $pull: { products: { product_id: productId } } }
+  )
+
+  req.flash('success', 'Đã xóa sản phẩm khỏi giỏ hàng!')
+  res.redirect(req.get('Referer') || '/')
+}
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -6,4 +6,7 @@ const controller = require('../../controllers/client/cart.controller')
 router.get('/', controller.cart)
 
 router.post('/add/:productId', controller.addPost)
+
+router.get('/delete/:productId', controller.delete)
+
 module.exports = router
