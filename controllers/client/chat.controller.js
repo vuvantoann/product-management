67da74623dfbb9e7f35484936607a24d7d77fc40
vuvantoann <toanvu1728@gamil.com>
2025-10-14 15:44:50 +0700

    Hiểm thị data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -4,6 +4,8 @@ const User = require('../../modals/user.modal')
 // controllers/client/service.controller.js
 module.exports.index = async (req, res) => {
   const userId = res.locals.user._id
+  const fullName = res.locals.user.fullName
+
   console.log(userId)
   _io.once('connection', (socket) => {
     socket.on('CLIENT_SEND_MESSAGE', async (content) => {
@@ -12,6 +14,18 @@ module.exports.index = async (req, res) => {
         content: content,
       })
       await chat.save()
+
+      const time = chat.createdAt.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+      })
+
+      _io.emit('SERVER_RETURN_MESSAGE', {
+        userId: userId,
+        fullName: fullName,
+        content: content,
+        time: time,
+      })
     })
   })
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,53 @@ if (formSendData) {
   })
 }
 //END CLIENT_SEND_MESSAGE
+
+//SERVER_RETURN_MESSAGE
+
+socket.on('SERVER_RETURN_MESSAGE', (data) => {
+  console.log('📩 Tin nhắn mới:', data)
+
+  const body = document.querySelector('.chat #messages')
+  const myId = document.querySelector('[my-id]').getAttribute('my-id')
+
+  // Tạo phần tử chứa tin nhắn
+  const div = document.createElement('div')
+  div.classList.add('chat-message')
+
+  const isSent = myId === data.userId // 👈 xác định tin mình gửi hay nhận
+  div.classList.add(isSent ? 'sent' : 'received')
+
+  // Render HTML giống Pug
+  if (!isSent) {
+    // 👉 Tin nhắn nhận
+    div.innerHTML = `
+      <img class="avatar" src="https://i.pravatar.cc/40?img=1" alt="${data.fullName}">
+      <div class="message-content">
+        <div class="message-name">${data.fullName}</div>
+        <div class="message-text">${data.content}</div>
+        <div class="message-time">${data.time}</div>
+      </div>
+    `
+  } else {
+    // 👉 Tin nhắn mình gửi
+    div.innerHTML = `
+      <div class="message-content">
+        <div class="message-text">${data.content}</div>
+        <div class="message-time">${data.time}</div>
+      </div>
+    `
+  }
+
+  // Gắn vào cuối danh sách tin nhắn
+  body.appendChild(div)
+
+  // Scroll xuống cuối cùng mỗi khi có tin nhắn mới
+  body.scrollTop = body.scrollHeight
+})
+
+//END SERVER_RETURN_MESSAGE
+
+const bodyChat = document.querySelector('.chat #messages')
+if (bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight
+}
